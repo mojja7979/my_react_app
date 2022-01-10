2ab0a940b4c0a6214aaced73813db5505a7f9db2
mjhan21 <mjhan@doctorringo.com>
2022-01-10 23:47:42 +0900

    init

---
+++ ch02_3/src/data/util.ts
@@ -0,0 +1,10 @@
+export const makeArray = (length: number) => new Array(length).fill(null);
+
+export const random = (min: number, max: number): number =>
+  Math.round(Math.random() * (max - min)) + min;
+
+export const unsplashUrl = (width: number, height: number): string =>
+  `http://source.unsplash.com/random/${width}x${height}`;
+
+export const avatarUriByName = (name: string) =>
+  `https://ui-avatars.com/api/?name=${name.split(' ').join('+')}`;
